558d88d731268198938c6d03b48441233c73d4bb
tan-developer <tan.developer@gmail.com>
2023-05-19 23:13:16 +0700

    new api/conversations/[conversationId]

--- app/api/get/route.ts
+++ app/api/get/route.ts
@@ -1,5 +1,6 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb"
 
 interface IParams {
   conversationId?: string;
@@ -9,6 +10,25 @@ export async function POST(request: Request, { params }: { params: IParams }) {
 
   try {
     const currentUser = await getCurrentUser();
+    const {conversationId} = params;
+
+    if(!currentUser?.email || !currentUser.id) {
+      return new NextResponse("Unauthorized" , {status : 401})
+    }
+
+    const conversation = await prisma.conversation.findUnique({
+      where : {
+        id : conversationId 
+      },
+      include : {
+        messages : {
+          include : {
+            seen : true
+          }
+        }
+      }
+    })
+
   } catch (error) {
     return new NextResponse("Internal Error" , {status : 500})
   }
